86c362487faf313d3167faa866da57e927d1a6b6
tinthanhtv2014 <tinthanhtv2014@gmail.com>
2024-04-30 20:44:27 +0700

    update api thong tin user

--- DBackendPhucShoe/DBackendPhucShoe/src/controllers/apiUserController.js
+++ DBackendPhucShoe/DBackendPhucShoe/src/controllers/apiUserController.js
@@ -3,6 +3,12 @@ const {
   createLoginUser,
   getUser,
   postLoginUser,
+  getThongtinUser,
+  updateUser,
+  DeleteUser,
+  postLoginAdmin,
+  createLoginAdmin,
+  updatePasswordUser,
 } = require("../services/apiCRUDServices");
 
 const CreateUser = async (req, res) => {
@@ -41,8 +47,102 @@ const loginUser = async (req, res) => {
   }
 };
 
+const getInfoUser = async (req, res) => {
+  try {
+    const taikhoan = req.params.username;
+
+    const results = await getThongtinUser(taikhoan);
+    return res.status(200).json({
+      EM: results.EM,
+      EC: results.EC,
+      DT: results.DT,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const CapnhatUser = async (req, res) => {
+  try {
+    const taikhoan = req.params.username;
+
+    const ten = req.body.ten;
+    const diachi = req.body.diachi;
+    const sodienthoai = req.body.sodienthoai;
+    const results = await updateUser(taikhoan, ten, diachi, sodienthoai);
+    return res.status(200).json({
+      EM: results.EM,
+      EC: results.EC,
+      DT: results.DT,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const CapnhatPasswordUser = async (req, res) => {
+  try {
+    const taikhoan = req.params.username;
+    const matkhaucu = req.body.passwordcu;
+    const matkhaumoi = req.body.passwordmoi;
+
+    const results = await updatePasswordUser(taikhoan, matkhaucu, matkhaumoi);
+    return res.status(200).json({
+      EM: results.EM,
+      EC: results.EC,
+      DT: results.DT,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const XoaUser = async (req, res) => {
+  try {
+    const taikhoan = req.params.username;
+    const results = await DeleteUser(taikhoan);
+    return res.status(200).json({
+      EM: results.EM,
+      EC: results.EC,
+      DT: results.DT,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+const loginAdmin = async (req, res) => {
+  try {
+    const username = req.body.username;
+    const password = req.body.password;
+    const results = await postLoginAdmin(username, password);
+    return res.status(200).json({
+      EM: results.EM,
+      EC: results.EC,
+      DT: results.DT,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+const registerAdmin = async (req, res) => {
+  const taikhoan = req.body.username;
+  const matkhau = req.body.password;
+  console.log(req.body);
+  const results = await createLoginAdmin(taikhoan, matkhau);
+  return res.status(200).json({
+    EM: results.EM,
+    EC: results.EC,
+    DT: results.DT,
+  });
+};
 module.exports = {
   CreateUser,
   getAllUser,
   loginUser,
+  getInfoUser,
+  CapnhatUser,
+  XoaUser,
+  loginAdmin,
+  registerAdmin,
+  CapnhatPasswordUser,
 };
--- DBackendPhucShoe/DBackendPhucShoe/src/services/apiCRUDServices.js
+++ DBackendPhucShoe/DBackendPhucShoe/src/services/apiCRUDServices.js
@@ -16,7 +16,9 @@ const checkPassword = (inputpassword, hashpass) => {
 };
 
 const getUser = async () => {
-  const [results, fields] = await connection.execute("SELECT * FROM `users`");
+  const [results, fields] = await connection.execute(
+    "SELECT u.*,k.makhachhang,k.ten, k.diachi,k.ghichu,k.sodienthoai FROM `users` as u, khachhang as k where u.taikhoan = k.taikhoan"
+  );
   return {
     EM: "xem thoong tin thanh cong",
     EC: 1,
@@ -39,10 +41,15 @@ const createLoginUser = async (taikhoan, password) => {
       `INSERT INTO users (taikhoan,matkhau) VALUES (?, ?)`,
       [taikhoan, matkhauHashed]
     );
+    let newId = generateRandomCustomerID();
+    const [results1, fields1] = await connection.execute(
+      "insert into khachhang (makhachhang,taikhoan) values (?,?)",
+      [newId, taikhoan]
+    );
     return {
       EM: "tạo tài khoản thành công",
       EC: 1,
-      DT: results,
+      DT: { results, results1 },
     };
   } catch (error) {
     console.error("Error creating login user:", error);
@@ -100,8 +107,269 @@ const postLoginUser = async (taikhoan, password) => {
     throw error;
   }
 };
+
+const getThongtinUser = async (taikhoan) => {
+  try {
+    const [results, fields] = await connection.execute(
+      "SELECT k.*,u.matkhau FROM `khachhang` as k, users as u where u.taikhoan = k.taikhoan and u.taikhoan = ?",
+      [taikhoan]
+    );
+    console.log(results);
+    if (results.length > 0) {
+      return {
+        EM: "tìm thấy user !!!",
+        EC: 1,
+        DT: results,
+      };
+    } else {
+      return {
+        EM: "user này không tồn tại",
+        EC: 0,
+        DT: [],
+      };
+    }
+  } catch (error) {
+    console.error("Error in postLoginUser:", error);
+    throw error;
+  }
+};
+
+const generateRandomCustomerID = () => {
+  return Math.floor(Math.random() * 1000) + 1;
+};
+
+const updateUser = async (taikhoan, ten, diachi, sodienthoai) => {
+  try {
+    const [results, fields] = await connection.execute(
+      "select * from users where taikhoan = ?",
+      [taikhoan]
+    );
+    console.log("check resultls", results);
+    if (results.length > 0) {
+      const [results2, fields] = await connection.execute(
+        "update khachhang set ten = ?, diachi = ?, sodienthoai = ? where taikhoan = ?",
+        [ten, diachi, sodienthoai, taikhoan]
+      );
+      return {
+        EM: "thay đổi thông tin thành công",
+        EC: 1,
+        DT: results2,
+      };
+    } else {
+      return {
+        EM: "Tài khoản không tồn tại",
+        EC: 0,
+        DT: [],
+      };
+    }
+    console.log(results);
+  } catch (error) {
+    console.error("Error in postLoginUser:", error);
+    throw error;
+  }
+};
+
+const updatePasswordUser = async (taikhoan, matkhaucu, matkhaumoi) => {
+  try {
+    const [results, fields] = await connection.execute(
+      "select * from users where taikhoan = ?",
+      [taikhoan]
+    );
+    if (results.length > 0) {
+      const isCorrectPass = await bcrypt.compare(matkhaucu, results[0].matkhau);
+      if (isCorrectPass) {
+        const matkhauHashed = hashPassword(matkhaumoi);
+        const [results1, fields] = await connection.execute(
+          "update users set matkhau = ? where taikhoan = ?",
+          [matkhauHashed, taikhoan]
+        );
+        return {
+          EM: "thay đổi mật khẩu thành công",
+          EC: 1,
+          DT: results1,
+        };
+      } else {
+        return {
+          EM: "không thể thay đổi mật khẩu",
+          EC: 0,
+          DT: [],
+        };
+      }
+    } else {
+      return {
+        EM: "Tài khoản không tồn tại",
+        EC: 0,
+        DT: [],
+      };
+    }
+    console.log(results);
+  } catch (error) {
+    console.error("Error in postLoginUser:", error);
+    throw error;
+  }
+};
+
+const DeleteUser = async (taikhoan) => {
+  try {
+    const [results, fields] = await connection.execute(
+      "delete from khachhang where taikhoan = ?",
+      [taikhoan]
+    );
+    const [results2, fields2] = await connection.execute(
+      "delete from users where taikhoan = ?",
+      [taikhoan]
+    );
+
+    return {
+      EM: "xóa tài khoản thành công !!!",
+      EC: 1,
+      DT: [],
+    };
+  } catch (error) {
+    console.error("Error in postLoginUser:", error);
+    throw error;
+  }
+};
+
+const UpdateAdmin = async (taikhoan, matkhau) => {
+  try {
+    let taikhoantontai = await checkTaiKhoanAdmin(taikhoan);
+    if (taikhoantontai === true) {
+      return {
+        EM: "tai khoan da ton tai",
+        EC: 0,
+        DT: [],
+      };
+    }
+    const matkhauHashed = hashPassword(password);
+    console.log(matkhauHashed);
+    const [results, fields] = await connection.execute(
+      `INSERT INTO admin (username,password) VALUES (?, ?)`,
+      [taikhoan, matkhauHashed]
+    );
+
+    return {
+      EM: "tạo tài khoản thành công",
+      EC: 1,
+      DT: { results },
+    };
+  } catch (error) {
+    console.error("Error creating login user:", error);
+    throw error;
+  }
+};
+
+const checkTaiKhoanAdmin = async (taikhoan) => {
+  try {
+    const [results, fields] = await connection.execute(
+      "SELECT * FROM `admin` where `username` = ?",
+      [taikhoan]
+    );
+
+    if (results.length > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//Login for admin
+const postLoginAdmin = async (username, password) => {
+  try {
+    console.log(username);
+    const [results, fields] = await connection.execute(
+      "SELECT * FROM `admin` where `username` = ?",
+      [username]
+    );
+    if (results.length > 0) {
+      const isCorrectPass = await bcrypt.compare(password, results[0].password);
+      if (isCorrectPass) {
+        return {
+          EM: "Đăng nhập thành công",
+          EC: 1,
+          DT: results,
+        };
+      } else {
+        return {
+          EM: "Mật khẩu không chính xác",
+          EC: 0,
+          DT: [],
+        };
+      }
+    } else {
+      return {
+        EM: "Tài khoản không tồn tại",
+        EC: 0,
+        DT: [],
+      };
+    }
+  } catch (error) {
+    console.error("Error in postLoginUser:", error);
+    throw error;
+  }
+};
+
+//create admin account
+const createLoginAdmin = async (taikhoan, password) => {
+  const username = taikhoan;
+
+  try {
+    let taikhoantontai = await checktaikhoanAdmin(username);
+    if (taikhoantontai === true) {
+      return {
+        EM: "tai khoan da ton tai",
+        EC: 0,
+        DT: [],
+      };
+    }
+    const matkhauHashed = hashPassword(password);
+    console.log(matkhauHashed);
+    const [results, fields] = await connection.execute(
+      `INSERT INTO admin (username,password) VALUES (?, ?)`,
+      [username, matkhauHashed]
+    );
+
+    return {
+      EM: "tạo tài khoản thành công",
+      EC: 1,
+      DT: { results },
+    };
+  } catch (error) {
+    console.error("Error creating login user:", error);
+    throw error;
+  }
+};
+
+//check admin
+const checktaikhoanAdmin = async (username) => {
+  try {
+    const [results, fields] = await connection.execute(
+      "SELECT * FROM `admin` where `taikhoan` = ?",
+      [username]
+    );
+
+    if (results.length > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 module.exports = {
   createLoginUser,
   getUser,
   postLoginUser,
+  getThongtinUser,
+  updateUser,
+  DeleteUser,
+  UpdateAdmin,
+  postLoginAdmin,
+  createLoginAdmin,
+  updatePasswordUser,
 };
